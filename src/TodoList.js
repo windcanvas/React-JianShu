6bf3fc5a6351d1c9991936c81d3c74cf49c98a07
liqilong <liqilong199126@gmail.com>
2019-06-03 01:17:35 +0800

    TodoItem与TodoList代码优化

---
+++ src/TodoList.js
@@ -0,0 +1,73 @@
+import React, {Component, Fragment} from 'react';
+import TodoItem from './TodoItem';
+import './style.css';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <label htmlFor="insertArea">输入内容</label>
+                    <input
+                        id="insertArea"
+                        className='input'
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange}
+                    />
+                    <button onClick={this.handleBtnClick}>提交</button>
+                </div>
+                <ul>
+                    {this.getTodoItem()}
+                </ul>
+            </Fragment>
+        );
+    }
+
+    getTodoItem() {
+        return this.state.list.map((item, index) => {
+            return (
+                <TodoItem
+                    content={item}
+                    index={index}
+                    key={index}
+                    deleteItem={this.handleItemDelete}
+                />
+            )
+        });
+    }
+
+    handleInputChange(e) {
+        const value = e.target.value;
+        this.setState(() => ({
+                inputValue: value
+        }));
+    }
+
+    handleBtnClick() {
+        this.setState(() => ({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        }));
+    }
+
+    handleItemDelete(index) {
+        this.setState(() => {
+            const list = [...this.state.list];
+            list.splice(index, 1);
+            return {list};
+        })
+    }
+}
+
+export default TodoList;
